ae5dcb52964933ed64713cde3299fde5fa98c8d2
Tripp Martin <tripp.martin3@gmail.com>
2020-08-12 13:47:40 -0400

    set up bootstrap, initial styling work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'home/landing'
   devise_for :users
-  root to: 'home#index'
+  root to: 'home#landing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
