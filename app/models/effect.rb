5f252040e269aa04b29b68be1511b92bbe9f4e37
Tripp Martin <tripp.martin3@gmail.com>
2020-08-13 16:36:54 -0400

    added annotate

--- app/models/effect.rb
+++ app/models/effect.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: effects
+#
+#  id           :bigint           not null, primary key
+#  datetime     :datetime
+#  mood         :integer
+#  side_effects :text             default([]), is an Array
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
 class Effect < ApplicationRecord
   belongs_to :user
   validates :datetime, presence: true
--- spec/models/effect_spec.rb
+++ spec/models/effect_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: effects
+#
+#  id           :bigint           not null, primary key
+#  datetime     :datetime
+#  mood         :integer
+#  side_effects :text             default([]), is an Array
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
 require 'rails_helper'
 
 RSpec.describe Effect, type: :model do
