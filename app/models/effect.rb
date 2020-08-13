4fe8a915627ae0231dadac4ba643896b66a5d209
Tripp Martin <tripp.martin3@gmail.com>
2020-08-13 16:45:53 -0400

    ran annotation

--- app/models/effect.rb
+++ app/models/effect.rb
@@ -12,6 +12,14 @@
 #  updated_at   :datetime         not null
 #  user_id      :bigint           not null
 #
+# Indexes
+#
+#  index_effects_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Effect < ApplicationRecord
   belongs_to :user
   validates :datetime, presence: true
--- spec/models/effect_spec.rb
+++ spec/models/effect_spec.rb
@@ -10,6 +10,14 @@
 #  updated_at   :datetime         not null
 #  user_id      :bigint           not null
 #
+# Indexes
+#
+#  index_effects_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Effect, type: :model do
