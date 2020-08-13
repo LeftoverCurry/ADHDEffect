826d3d082a1fead16e4d197fe79683fdcdebdae1
Tripp Martin <tripp.martin3@gmail.com>
2020-08-13 13:43:18 -0400

    added redirection for signed in user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
-  def landing
+  before_action :check_signed_in
+
+  def landing; end
+
+  private
+
+  def check_signed_in
+    redirect_to home_path if signed_in?
   end
 end
