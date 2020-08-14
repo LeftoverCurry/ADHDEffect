d0309da2f76dbee8a18f4cb5c1bacac0cc2a9df6
Tripp Martin <tripp.martin3@gmail.com>
2020-08-14 18:18:39 -0400

    moved chart logic into helper

--- app/helpers/effects_helper.rb
+++ app/helpers/effects_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module EffectsHelper
+  def build_mood_chart
+    data_hash = {}
+    @user_effects.each { |record| data_hash[record.datetime] = record.mood }
+    data_hash
+  end
 end
